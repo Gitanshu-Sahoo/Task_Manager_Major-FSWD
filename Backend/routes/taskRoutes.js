1d49344740b33f88d77b588e9d59ee2166fddd56
Gitanshu Sahoo <madefor1mc@gmail.com>
2026-06-30 00:20:39 +0530

    Fix import case

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Task = require("../models/Task");
+const Task = require("../models/task");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // GET all tasks of logged-in user
